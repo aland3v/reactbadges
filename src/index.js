24fe1d68f8021177aa29754fb44dfd328a1bc635
Alan dev <aland3v@gmail.com>
2021-03-02 22:49:48 -0400

    ReacDOM.render()->appendChild() y React->createElement()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/*const element = document.createElement('h1');
+element.innerText = 'Hello, Platzi Badges';
+const container = document.getElementById('app');
+container.appendChild(element);*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
