6f4e468426decc39704ee8e5cbdef9377b42803b
Alan dev <aland3v@gmail.com>
2021-03-13 22:30:20 -0400

    Hooks y custom hooks de react

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -5,7 +5,19 @@ import Badge from "../component/Badge";
 import DeleteBadgeModal from "../component/DeleteBadgeModal";
 import { Link } from "react-router-dom";
 
+/**
+ * Custom hook
+ */
+function useIncreaseCount(max) {
+    const [count, setCount] = React.useState(0);
+    if (count > max) {
+        setCount(0);
+    }
+    return [count, setCount];
+}
+
 function BadgeDetails(props) {
+    const [count, setCount] = useIncreaseCount(4);
     const badge = props.badge;
     return (
         <div>
@@ -38,6 +50,9 @@ function BadgeDetails(props) {
                         <h2>Actions</h2>
                         <div>
                             <div>
+                                <button onClick={() => setCount(count + 1)} className="btn btn-primary mr-4">
+                                    Increase Count: {count}
+                                </button>
                                 <Link
                                     className="btn btn-primary mb-4"
                                     to={`/badges/${badge.id}/edit`}>
